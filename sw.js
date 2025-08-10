8dc694b65e18f28db3b1517811b5e2fcf03c0bec
jakubjvecera <100529442+jakubjvecera@users.noreply.github.com>
2025-08-10 21:38:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,16 +7,29 @@ const urlsToCache = [
   './placeholder-mapa.svg'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting(); // rychlejší přechod do aktivního stavu
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim(); // přebere kontrolu nad klienty ihned po aktivaci
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
