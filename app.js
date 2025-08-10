4197b69eaa9231f61219fed4d3a5a10b80a96c5d
jakubjvecera <100529442+jakubjvecera@users.noreply.github.com>
2025-08-10 18:26:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ if (batteryReplaceBtn) {
     let count = parseInt(batterySpan.textContent, 10);
     if (count > 0) {
       addBattery(-1);
+batterySpan.textContent = count - 1;
       hideBatteryReplaceButton();
       flashlightBtn.classList.add('active');
       flashlightActive = true;
